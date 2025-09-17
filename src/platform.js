58e84ce22f700a1e9ff795c0c298627ec3680e30
J.Soon <serdeemail@gmail.com>
2025-09-17 23:06:58 +0800

    ref: isElectron强制返回false

--- src/platform.js
+++ src/platform.js
@@ -1,21 +1,21 @@
 // 平台相关代码，目前主要用来处理electron 和 浏览器之间不同
 
 export function isElectron() {
-    // 如果用 electron 加载网页端，本函数要强制返回 false；否则，环境会判断错误，导致无法正常加载网页端。
-    // Renderer process
-    if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
-        return true;
-    }
+    // // 如果用 electron 加载网页端，本函数要强制返回 false；否则，环境会判断错误，导致无法正常加载网页端。
+    // // Renderer process
+    // if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
+    //     return true;
+    // }
 
-    // Main process
-    if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
-        return true;
-    }
+    // // Main process
+    // if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
+    //     return true;
+    // }
 
-    // Detect the user agent when the `nodeIntegration` option is set to true
-    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
-        return true;
-    }
+    // // Detect the user agent when the `nodeIntegration` option is set to true
+    // if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+    //     return true;
+    // }
 
     return false;
 }
